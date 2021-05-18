959f8a00d25a6899ab95884cb0fd2e3850ec6b1f
rwv <rwv@gmail.com>
2021-05-18 00:05:55 +0000

    use sendmail

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -54,16 +54,8 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      domain: "gmail.com",
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: ENV["AGATHA_USERNAME"],
-      password: ENV["AGATHA_PASSWORD"]
-  }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.sendmail_settings = { location: '/usr/bin/mail', arguments: '-i -t'}  
   config.action_mailer.default_url_options = { host: "localhost:3000" }
   
   config.i18n.fallbacks = true
